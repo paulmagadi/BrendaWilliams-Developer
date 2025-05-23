436f69877abccc9801b424d3a29f544782180d89
SQCodes <paulsqmagadi@gmail.com>
2025-05-23 12:33:00 +0300

    new file:   LICENCE 	new file:   icons/arrow_drop_down_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/chat_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/chevron_right_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/close_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/code_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/contacts_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/forum_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/home_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/mail_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/menu_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/person_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/send_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/settings_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/work_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/x-twitter-brands-solid.svg 	new file:   images/WhatsAppButtonWhiteSmall.svg 	new file:   images/brenda.jpg 	new file:   images/icons8-twitterx.svg 	new file:   images/paul001.png 	new file:   images/paulsqmagadi@gmail.com.pdf 	new file:   images/project-1.jpg 	new file:   images/project-2.png 	new file:   images/project-3.jpg 	new file:   images/project-4.png 	new file:   images/project-5.png 	new file:   images/project-6.png 	new file:   images/project-7.png 	new file:   images/project-8.jpg 	new file:   images/project-9.png 	new file:   index.html 	new file:   responsive.css 	new file:   scripts.js 	new file:   styles.css

---
+++ scripts.js
@@ -0,0 +1,92 @@
+
+menuBar = document.querySelector('.menu-bar');
+sideBar = document.querySelector('.sidebar');
+exitSidebarBtn = document.querySelector('.exit-sidebar');
+
+// Toggle sidebar 
+menuBar.addEventListener('click', function() {
+    sideBar.classList.toggle('open');
+});
+
+// Exit sidebar 
+exitSidebarBtn.addEventListener('click', function() {
+    sideBar.classList.toggle('open');
+    menuBar.style.display = 'flex';
+});
+
+
+const navBtns = document.querySelectorAll('.menu');
+const sections = document.querySelectorAll('section');
+
+// Show different page sections on clicking the navigation
+function showSection(section) {
+    Object.values(navBtns).forEach(btn => btn.classList.remove('active'));
+    navBtns[section].classList.add('active');
+    sideBar.classList.toggle('open');
+}
+
+Object.keys(navBtns).forEach(section => {
+    navBtns[section].addEventListener('click', () => showSection(section));
+});
+
+// Activate active class on the navbar links when scrolling the sections
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+
+    sections.forEach(section => {
+        let sectionTop = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+
+        if (sectionTop >= offset && sectionTop < offset + height) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navBtns.forEach(menu => {
+        menu.classList.remove('active');
+        if (menu.querySelector(`a[href="#${currentSection}"]`)) {
+            menu.classList.add('active');
+        }
+    });
+});
+
+// Project section 
+const projectMenuItems = document.querySelectorAll('.project-menu');
+
+const projectCategories = {
+    webDesign: document.querySelectorAll('.web-design'),
+    webDev: document.querySelectorAll('.web-dev'),
+    appDesign: document.querySelectorAll('.app-design'),
+    appDev: document.querySelectorAll('.app-dev'),
+};
+
+
+// Toggle different project categories 
+function showProjects(category) {
+    if (category === 'all') {
+        Object.values(projectCategories).forEach(items => items.forEach(item => item.style.display = 'flex'));
+    } else {
+        Object.values(projectCategories).forEach(items => items.forEach(item => item.style.display = 'none'));
+        projectCategories[category].forEach(item => item.style.display = 'flex');
+    }
+
+    // Toggle the active class on the menu links 
+    projectMenuItems.forEach(menu => menu.classList.remove('active-project'));
+    document.querySelector(`[data-category="${category}"]`).classList.add('active-project');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showProjects('appDev');
+});
+
+projectMenuItems.forEach(menu => {
+    menu.addEventListener('click', () => {
+        const category = menu.getAttribute('data-category');
+        showProjects(category);
+    });
+});
+
+
+
+
